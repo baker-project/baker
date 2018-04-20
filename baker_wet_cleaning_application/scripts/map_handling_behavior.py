e2fed221d4f90edd385172b698452bd75dcf15b3
Martin Finkbeiner <martin.finkbeiner@ipa.fraunhofer.de>
2018-04-20 11:42:38 +0200

    Changed movement_handling_behavior to support serveral different cleaning types one day

--- baker_wet_cleaning_application/scripts/map_handling_behavior.py
+++ baker_wet_cleaning_application/scripts/map_handling_behavior.py
@@ -23,14 +23,18 @@ class MapHandlingBehavior(behavior_container.BehaviorContainer):
 	
 	# Method for returning to the standard pose of the robot
 	def setParameters(self, robot_radius, database_handler, is_wet=False, is_overdue=False):
+		# Parameters set autonomously
 		self.map_receiving_service_str_ = '/map_management_client/get_map_image'
 		self.map_segmented_receiving_service_str_ = '/map_management_client/get_map_segmented_image'
 		self.map_segmentation_service_str_ = '/room_segmentation/room_segmentation_server'
 		self.room_sequencing_service_str_ = '/room_sequence_planning/room_sequence_planning_server'
-		self.robot_radius_ = robot_radius
+		# Parameters set from the outside
 		self.database_handler_ = database_handler
 		self.is_wet_ = is_wet
 		self.is_overdue_ = is_overdue
+		# DEPRECATED, use database in future
+		self.robot_radius_ = robot_radius
+
 
 	# Method for returning to the standard pose of the robot
 	def returnToRobotStandardState(self):
@@ -76,34 +80,42 @@ class MapHandlingBehavior(behavior_container.BehaviorContainer):
 		self.map_segmenter_.executeCustomBehavior()
 		self.segmentation_data_ = self.map_segmenter_.segmentation_result_
 		"""
+		
 
-		# Get a segmented map and RoomInformationArray from the database handler
-		self.segmented_map_, self.room_information_in_pixel_ = self.database_handler_.getMapAndRoomInformationInPixel(self.database_handler_.due_rooms_cleaning_)
-
+		# Extract all rooms out of the database which are relevant
 		if (self.is_overdue_ == False):
 			self.database_handler_.getAllDueRooms()
 			rooms_trashcan_only, rooms_dry_cleaning, rooms_wet_cleaning = self.database_handler_.sortRoomsList(self.database_handler_.due_rooms_)
 		else:
-			self.database_handler_.getAllDueRooms()
+			self.database_handler_.getAllOverdueRooms()
 			rooms_trashcan_only, rooms_dry_cleaning, rooms_wet_cleaning = self.database_handler_.sortRoomsList(self.database_handler_.overdue_rooms_)
 
+		# Merge the trashcan rooms together with the dry rooms --> Use rooms_dry_cleaning from here
+		for room in rooms_trashcan_only:
+			if not (room in rooms_dry_cleaning):
+				rooms_dry_cleaning.append(room) 
 		
-		# Room sequencing
+		# Get the parameters for room sequencing
+		if (self.is_overdue_ == False):
+			if (self.is_wet_ == False):
+				self.segmented_map_, self.room_information_in_pixel_ = self.database_handler_.getMapAndRoomInformationInPixel(rooms_wet_cleaning)
+			else:
+				self.segmented_map_, self.room_information_in_pixel_ = self.database_handler_.getMapAndRoomInformationInPixel(rooms_dry_cleaning)
+		else:
+			if (self.is_wet_ == False):
+				self.segmented_map_, self.room_information_in_pixel_ = self.database_handler_.getMapAndRoomInformationInPixel(rooms_wet_cleaning)
+			else:
+				self.segmented_map_, self.room_information_in_pixel_ = self.database_handler_.getMapAndRoomInformationInPixel(rooms_dry_cleaning)
+
+		# Sequence the rooms
 		self.room_sequencer_ = room_sequencing_behavior.RoomSequencingBehavior("Room sequencing", self.interrupt_var_, self.room_sequencing_service_str_)
 		self.room_sequencer_.setParameters(
-			#self.map_data_, 
-			#self.segmentation_data_,
 			self.database_handler_,
 			self.room_information_in_pixel_,
 			self.robot_radius_
 			)
 		self.room_sequencer_.executeCustomBehavior()
-		self.room_sequencing_data_ = self.room_sequencer_.room_sequence_result_
-		
-		# Interruption opportunity
-		if self.handleInterrupt() == 2:
-			return
-			
+		self.room_sequencing_data_ = self.room_sequencer_.room_sequence_result_	
 		#self.printMsg("self.room_sequencing_data_.checkpoints=" + str(self.room_sequencing_data_.checkpoints))
 		#self.printMsg("self.segmentation_data_.room_information_in_pixel=" + str(self.segmentation_data_.room_information_in_pixel))
 		#self.printMsg("self.segmentation_data_.room_information_in_meter=" + str(self.segmentation_data_.room_information_in_meter))
--- baker_wet_cleaning_application/scripts/movement_handling_behavior.py
+++ baker_wet_cleaning_application/scripts/movement_handling_behavior.py
@@ -30,8 +30,8 @@ class MovementHandlingBehavior(behavior_container.BehaviorContainer):
 
 		
 	# Method for setting parameters for the behavior
-	def setParameters(self, database_handler, segmented_map, room_information_in_meter, sequence_data, robot_frame_id, robot_radius, coverage_radius, field_of_view):
-		# Service strings
+	def setParameters(self, database_handler, segmented_map, room_information_in_meter, sequence_data, robot_frame_id, robot_radius, coverage_radius, field_of_view, is_wet=False):
+		# Parameters set autonomously
 		self.room_exploration_service_str_ = '/room_exploration/room_exploration_server'
 		self.move_base_path_service_str_ = '/move_base_path'
 		self.move_base_wall_follow_service_str_ = '/move_base_wall_follow'
@@ -40,11 +40,14 @@ class MovementHandlingBehavior(behavior_container.BehaviorContainer):
 		self.stop_cleaning_service_str_ = '/brush_cleaning_module_interface/stop_brush_cleaner'
 		self.coverage_monitor_dynamic_reconfigure_service_str_ = '/room_exploration/coverage_monitor_server'
 		self.stop_coverage_monitoring_service_str_ = "/room_exploration/coverage_monitor_server/stop_coverage_monitoring"
+		# Parameters set from the outside
 		self.database_handler_= database_handler
+		self.sequence_data_ = sequence_data
+		self.is_wet_ = is_wet
+		# DEPRECATED Use database handler
 		#self.segmentation_data_ = segmentation_data
 		self.segmented_map_ = segmented_map
 		self.room_information_in_meter_ = room_information_in_meter
-		self.sequence_data_ = sequence_data
 		self.robot_frame_id_ = robot_frame_id
 		self.robot_radius_ = robot_radius
 		self.coverage_radius_ = coverage_radius
@@ -66,168 +69,193 @@ class MovementHandlingBehavior(behavior_container.BehaviorContainer):
 		self.path_follower_ = move_base_path_behavior.MoveBasePathBehavior("MoveBasePathBehavior_PathFollowing", self.interrupt_var_, self.move_base_path_service_str_)
 		self.wall_follower_ = move_base_path_behavior.MoveBasePathBehavior("MoveBasePathBehavior_WallFollowing", self.interrupt_var_, self.move_base_wall_follow_service_str_)
 
+
+
+		# TOOL CHANGE ACCORDING TO CLEANING TASK
+		# ======================================
+
 		for current_checkpoint_index in range(len(self.sequence_data_.checkpoints)):
-			for current_room_index in self.sequence_data_.checkpoints[current_checkpoint_index].room_indices:
 
 
-				self.printMsg("Attending to next room with current_room_index=" + str(current_room_index))
 
-				# Interruption opportunity
-				if self.handleInterrupt() == 2:
-					return
+			# TROLLEY MOVEMENT TO CHECKPOINT
+			# ==============================
 
-				# Room exploration
-				"""
-				For room exploration:
-				input_map = self.getMapSegmentAsImageMsg(current_room_index)
-				map_resolution = self.database_handler_.database_.global_map_data_.map_resolution_
-				map_origin = self.database_handler_.database_.global_map_data_.map_origin_
-				robot_radius = 0.325
-				coverage_radius = 0.25
-				field_of_view = [Point32(x=0.04035, y=0.136), Point32(x=0.04035, y=-0.364), Point32(x=0.54035, y=-0.364), Point32(x=0.54035, y=0.136)] # this field of view represents the off-center iMop floor wiping device
-				starting_position = Pose2D(x=1., y=0., theta=0.)
-				planning_mode = 2
-				"""
-				current_room_center = self.room_information_in_meter_[current_room_index].room_center
-				current_room_map = self.getMapSegmentAsImageMsg(self.opencv_segmented_map_, current_room_index)
-				self.room_explorer_.setParameters(
-					current_room_map,
-					self.database_handler_.database_.global_map_data_.map_resolution_,
-					self.database_handler_.database_.global_map_data_.map_origin_,
-					robot_radius = self.robot_radius_,
-					coverage_radius = self.coverage_radius_,
-					field_of_view = self.field_of_view_,		# this field of view represents the off-center iMop floor wiping device
-					starting_position = Pose2D(x=current_room_center.x, y=current_room_center.y, theta=0.),	# todo: determine current robot position
+
+
+			# IN CASE OF WET CLEANING
+			# =======================
+
+			if (self.is_wet_ == True):
+				for current_room_index in self.sequence_data_.checkpoints[current_checkpoint_index].room_indices:
+
+
+					self.printMsg("Attending to next room with current_room_index=" + str(current_room_index))
+
+					# Interruption opportunity
+					if self.handleInterrupt() == 2:
+						return
+
+					# Room exploration
+					"""
+					For room exploration:
+					input_map = self.getMapSegmentAsImageMsg(current_room_index)
+					map_resolution = self.database_handler_.database_.global_map_data_.map_resolution_
+					map_origin = self.database_handler_.database_.global_map_data_.map_origin_
+					robot_radius = 0.325
+					coverage_radius = 0.25
+					field_of_view = [Point32(x=0.04035, y=0.136), Point32(x=0.04035, y=-0.364), Point32(x=0.54035, y=-0.364), Point32(x=0.54035, y=0.136)] # this field of view represents the off-center iMop floor wiping device
+					starting_position = Pose2D(x=1., y=0., theta=0.)
 					planning_mode = 2
-				)
-				self.room_explorer_.executeBehavior()
-
-				# If no trajectory was created - move on to next room
-				if (self.room_explorer_.exploration_result_ == None):
-					continue
-				
-				# Interruption opportunity
-				if self.handleInterrupt() == 2:
-					return
-	
-				#rospy.sleep(20)
-				#continue
-
-				# Robot movement into next room
-				"""
-				For movement to room:
-				goal_position = self.segmentation_data_.room_information_in_meter[current_room_index].room_center
-				goal_orientation = Quaternion(x=0., y=0., z=0., w=0.)
-				header_frame_id = 'base_link'
-				"""
-				self.printMsg("Moving to room_center in meter=" + str(self.room_information_in_meter_[current_room_index].room_center))
-				self.move_base_handler_.setParameters(
-					self.room_information_in_meter_[current_room_index].room_center,
-					Quaternion(x=0., y=0., z=0., w=0.),	# todo: normalized quaternion
-					'base_link'
+					"""
+					current_room_center = self.room_information_in_meter_[current_room_index].room_center
+					current_room_map = self.getMapSegmentAsImageMsg(self.opencv_segmented_map_, current_room_index)
+					self.room_explorer_.setParameters(
+						current_room_map,
+						self.database_handler_.database_.global_map_data_.map_resolution_,
+						self.database_handler_.database_.global_map_data_.map_origin_,
+						robot_radius = self.robot_radius_,
+						coverage_radius = self.coverage_radius_,
+						field_of_view = self.field_of_view_,		# this field of view represents the off-center iMop floor wiping device
+						starting_position = Pose2D(x=current_room_center.x, y=current_room_center.y, theta=0.),	# todo: determine current robot position
+						planning_mode = 2
 					)
-				self.move_base_handler_.executeBehavior()
+					self.room_explorer_.executeBehavior()
+
+					# If no trajectory was created - move on to next room
+					if (self.room_explorer_.exploration_result_ == None):
+						continue
 				
-				# Interruption opportunity
-				if self.handleInterrupt() == 2:
-					return
-				
-				# baker_brush_cleaning_module_interface: turn on the cleaning device (service "start_brush_cleaner")
-				'''
-				self.printMsg("Start cleaning with " + self.start_cleaning_service_str_)
-				rospy.wait_for_service(self.start_cleaning_service_str_) 
-				try:
-					req = rospy.ServiceProxy(self.start_cleaning_service_str_, std_srvs.srv.Trigger)
-					resp = req()
-					print "Start cleaning returned with success status " + str(resp.success)
-				except rospy.ServiceException, e:
-					print "Service call to " + self.start_cleaning_service_str_ + " failed: %s" % e
-				'''
-				
-				# coverage_monitor_server: set the robot configuration (robot_radius, coverage_radius, coverage_offset) with dynamic reconfigure
-				#                          and turn on logging of the cleaned path (service "start_coverage_monitoring")
-				try:
-					print "Calling dynamic reconfigure at the coverage_monitor_server to set robot radius, coverage_radius, and coverage offset and start coverage monitoring."
-					client = dynamic_reconfigure.client.Client(self.coverage_monitor_dynamic_reconfigure_service_str_, timeout=5)
-					rospy.wait_for_service(self.coverage_monitor_dynamic_reconfigure_service_str_ + "/set_parameters")
-					client.update_configuration({"map_frame":self.database_handler_.database_.global_map_data_.map_header_frame_id_, "robot_frame":self.robot_frame_id_,
-												 "coverage_radius":self.coverage_radius_,
-												 "coverage_circle_offset_transform_x":0.5*(self.field_of_view_[0].x+self.field_of_view_[2].x),
-												 "coverage_circle_offset_transform_y":0.5*(self.field_of_view_[0].y+self.field_of_view_[1].y),
-												 "coverage_circle_offset_transform_z":0.0,
-												 "robot_trajectory_recording_active":True})
-				except rospy.ServiceException, e:
-					print "Dynamic reconfigure request to " + self.coverage_monitor_dynamic_reconfigure_service_str_ + " failed: %s" % e
-				
-				# Explored path follow
-				"""
-				For path follow movement:
-				target_poses = exploration_result.coverage_path_pose_stamped
-				area_map = current_room_map
-				path_tolerance = 0.2
-				goal_position_tolerance = 0.5
-				goal_angle_tolerance = 1.57
-				"""
-				self.path_follower_.setParameters(
-					self.room_explorer_.exploration_result_.coverage_path_pose_stamped,
-					current_room_map,
-					0.2,
-					0.5,
-					1.57
-				)
-				self.path_follower_.executeBehavior()
-				
-				# Interruption opportunity
-				if self.handleInterrupt() == 2:
-					return
+					# Interruption opportunity
+					if self.handleInterrupt() == 2:
+						return
 	
-				# Wall follow
-				"""
-				For wall following movement:
-				target_poses = exploration_result.coverage_path_pose_stamped
-				path_tolerance = 0.2
-				goal_position_tolerance = 0.4
-				goal_angle_tolerance = 3.14
-				"""
-				'''
-				self.wall_follower_.setParameters(
-					self.room_explorer_.exploration_result_.coverage_path_pose_stamped,
-					current_room_map,
-					0.2,
-					0.4,
-					3.14
-				)
-				self.wall_follower_.executeBehavior()
-				'''
-				
-				# Interruption opportunity
-				if self.handleInterrupt() == 2:
-					return
+					#rospy.sleep(20)
+					#continue
 
-				# Mark the current room as finished
-				self.database_handler_.checkoutCompletedRoom(self.database_handler_.due_rooms_cleaning_[self.sequence_data_.checkpoints[current_checkpoint_index].room_indices[current_room_index]], False, False)
-
-				# coverage_monitor_server.cpp: turn off logging of the cleaned path (service "stop_coverage_monitoring")
-				self.printMsg("Stop coverage monitoring with " + self.stop_coverage_monitoring_service_str_)
-				rospy.wait_for_service(self.stop_coverage_monitoring_service_str_) 
-				try:
-					req = rospy.ServiceProxy(self.stop_coverage_monitoring_service_str_, std_srvs.srv.Trigger)
-					resp = req()
-					print "Stop coverage monitoring returned with success status " + str(resp.success)
-				except rospy.ServiceException, e:
-					print "Service call to " + self.stop_coverage_monitoring_service_str_ + " failed: %s" % e
+					# Robot movement into next room
+					"""
+					For movement to room:
+					goal_position = self.segmentation_data_.room_information_in_meter[current_room_index].room_center
+					goal_orientation = Quaternion(x=0., y=0., z=0., w=0.)
+					header_frame_id = 'base_link'
+					"""
+					self.printMsg("Moving to room_center in meter=" + str(self.room_information_in_meter_[current_room_index].room_center))
+					self.move_base_handler_.setParameters(
+						self.room_information_in_meter_[current_room_index].room_center,
+						Quaternion(x=0., y=0., z=0., w=0.),	# todo: normalized quaternion
+						'base_link'
+					)
+					self.move_base_handler_.executeBehavior()
 				
-				'''
-				# baker_brush_cleaning_module_interface: turn off the cleaning device (service "stop_brush_cleaner")
-				self.printMsg("Stop cleaning with " + self.stop_cleaning_service_str_)
-				rospy.wait_for_service(self.stop_cleaning_service_str_) 
-				try:
-					req = rospy.ServiceProxy(self.stop_cleaning_service_str_, std_srvs.srv.Trigger)
-					resp = req()
-					print "Stop cleaning returned with success status " + str(resp.success)
-				except rospy.ServiceException, e:
-					print "Service call to " + self.stop_cleaning_service_str_ + " failed: %s" % e
-				'''
+					# Interruption opportunity
+					if self.handleInterrupt() == 2:
+						return
+				
+					# baker_brush_cleaning_module_interface: turn on the cleaning device (service "start_brush_cleaner")
+					'''
+					self.printMsg("Start cleaning with " + self.start_cleaning_service_str_)
+					rospy.wait_for_service(self.start_cleaning_service_str_) 
+					try:
+						req = rospy.ServiceProxy(self.start_cleaning_service_str_, std_srvs.srv.Trigger)
+						resp = req()
+						print "Start cleaning returned with success status " + str(resp.success)
+					except rospy.ServiceException, e:
+						print "Service call to " + self.start_cleaning_service_str_ + " failed: %s" % e
+					'''
+				
+					# coverage_monitor_server: set the robot configuration (robot_radius, coverage_radius, coverage_offset) with dynamic reconfigure
+					#                          and turn on logging of the cleaned path (service "start_coverage_monitoring")
+					try:
+						print "Calling dynamic reconfigure at the coverage_monitor_server to set robot radius, coverage_radius, and coverage offset and start coverage monitoring."
+						client = dynamic_reconfigure.client.Client(self.coverage_monitor_dynamic_reconfigure_service_str_, timeout=5)
+						rospy.wait_for_service(self.coverage_monitor_dynamic_reconfigure_service_str_ + "/set_parameters")
+						client.update_configuration({"map_frame":self.database_handler_.database_.global_map_data_.map_header_frame_id_, "robot_frame":self.robot_frame_id_,
+												 	"coverage_radius":self.coverage_radius_,
+												 	"coverage_circle_offset_transform_x":0.5*(self.field_of_view_[0].x+self.field_of_view_[2].x),
+												 	"coverage_circle_offset_transform_y":0.5*(self.field_of_view_[0].y+self.field_of_view_[1].y),
+												 	"coverage_circle_offset_transform_z":0.0,
+												 	"robot_trajectory_recording_active":True})
+					except rospy.ServiceException, e:
+						print "Dynamic reconfigure request to " + self.coverage_monitor_dynamic_reconfigure_service_str_ + " failed: %s" % e
+				
+					# Explored path follow
+					"""
+					For path follow movement:
+					target_poses = exploration_result.coverage_path_pose_stamped
+					area_map = current_room_map
+					path_tolerance = 0.2
+					goal_position_tolerance = 0.5
+					goal_angle_tolerance = 1.57
+					"""
+					self.path_follower_.setParameters(
+						self.room_explorer_.exploration_result_.coverage_path_pose_stamped,
+						current_room_map,
+						0.2,
+						0.5,
+						1.57
+					)
+					self.path_follower_.executeBehavior()
+				
+					# Interruption opportunity
+					if self.handleInterrupt() == 2:
+						return
+	
+					# Wall follow
+					"""
+					For wall following movement:
+					target_poses = exploration_result.coverage_path_pose_stamped
+					path_tolerance = 0.2
+					goal_position_tolerance = 0.4
+					goal_angle_tolerance = 3.14
+					"""
+					'''
+					self.wall_follower_.setParameters(
+						self.room_explorer_.exploration_result_.coverage_path_pose_stamped,
+						current_room_map,
+						0.2,
+						0.4,
+						3.14
+					)
+					self.wall_follower_.executeBehavior()
+					'''
+				
+					# Interruption opportunity
+					if self.handleInterrupt() == 2:
+						return
+
+					# Mark the current room as finished
+					self.database_handler_.checkoutCompletedRoom(self.database_handler_.due_rooms_cleaning_[self.sequence_data_.checkpoints[current_checkpoint_index].room_indices[current_room_index]], False, False)
+
+					# coverage_monitor_server.cpp: turn off logging of the cleaned path (service "stop_coverage_monitoring")
+					self.printMsg("Stop coverage monitoring with " + self.stop_coverage_monitoring_service_str_)
+					rospy.wait_for_service(self.stop_coverage_monitoring_service_str_) 
+					try:
+						req = rospy.ServiceProxy(self.stop_coverage_monitoring_service_str_, std_srvs.srv.Trigger)
+						resp = req()
+						print "Stop coverage monitoring returned with success status " + str(resp.success)
+					except rospy.ServiceException, e:
+						print "Service call to " + self.stop_coverage_monitoring_service_str_ + " failed: %s" % e
+				
+					'''
+					# baker_brush_cleaning_module_interface: turn off the cleaning device (service "stop_brush_cleaner")
+					self.printMsg("Stop cleaning with " + self.stop_cleaning_service_str_)
+					rospy.wait_for_service(self.stop_cleaning_service_str_) 
+					try:
+						req = rospy.ServiceProxy(self.stop_cleaning_service_str_, std_srvs.srv.Trigger)
+						resp = req()
+						print "Stop cleaning returned with success status " + str(resp.success)
+					except rospy.ServiceException, e:
+						print "Service call to " + self.stop_cleaning_service_str_ + " failed: %s" % e
+					'''
+
+
+
+			# IN CASE OF DRY CLEANING + TRASHCAN
+			# ==================================
+
+			if (self.is_wet_ == False):
+				pass
 
 
 	# Method for returning the segment of the map corresponding to the order number as cv_bridge
