5283332e01ea1cfcc544c3f35f5c74ffd0a023e1
Martin Finkbeiner <martin.finkbeiner@ipa.fraunhofer.de>
2018-07-21 00:51:03 +0200

    Fixed bug in database_handler.py and developed a database to csv conversion tool. Completed that tool.

--- baker_database/json_to_plan_converter.py
+++ baker_database/json_to_plan_converter.py
@@ -21,14 +21,44 @@ class JSONToCSVEncoder():
     def createRoomBook(self):
         file = open(str(self.csv_file_path_) + "/csv/ROOMBOOK.csv", "w")
         for room in self.database_.rooms_:
-            pass
+            file.write(str(room.room_position_id_) + ";")
+            file.write(str(room.room_floor_id_) + ";")
+            file.write(str(room.room_building_id_) + ";")
+            file.write(str(room.room_id_) + ";")
+            file.write(str(room.room_name_) + ";")
+            file.write("Reinigungsbereich;")
+            file.write(str(room.room_surface_type_) + ";")
+            file.write(str(room.room_cleaning_method_) + ";")
+            file.write(str(room.room_surface_area_) + ";")
+            file.write(str(room.room_trashcan_count_) + ";")
+            file.write(";")
+            file.write("?;")
+            file.write("?;")
+            file.write("?;")
+            file.write("?;")
+            file.write("Woche;")
+            file.write("\n")
+        file.close()
 
 
     # Method to create a terrytory plan out of the database files.
     def createTerritoryPlan(self):
         file = open(str(self.csv_file_path_) + "/csv/TERRITORYPLAN.csv", "w")
         for room in self.database_.rooms_:
-            pass
+			file.write(str(room.room_territory_id_) + ";")
+			file.write(str(room.room_position_id_) + ";")
+			file.write(str(room.room_floor_id_) + ";")
+			file.write(str(room.room_id_) + ";")
+			file.write("Bezeichnung 1;")
+			file.write("Bezeichnung 2;")
+			file.write("Raumgruppe;")
+			file.write("Bezeichnung 3;")
+			file.write(str(room.room_surface_type_) + ";")
+			file.write(str(room.room_surface_area_) + ";")
+			file.write("INTERVAL_STRING;")
+			file.write(str(";".join(room.room_scheduled_days_)) + ";")
+			file.write("\n")
+        file.close()
 
 
     # Public method which creates a roombook and a territory plan from the stated database files. 
