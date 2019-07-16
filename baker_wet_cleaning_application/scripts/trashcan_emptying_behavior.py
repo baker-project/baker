86ef6f8223584ed3fdac7dbf7d7614cd812fb37a
Marc-AntoineA <marc-antoine.auge@eleves.enpc.fr>
2019-07-16 16:03:22 +0200

    WIP - updates trashcan emptying behavior to use the robotic arm (basic). It works

--- baker_wet_cleaning_application/scripts/trashcan_emptying_behavior.py
+++ baker_wet_cleaning_application/scripts/trashcan_emptying_behavior.py
@@ -2,9 +2,13 @@
 
 import behavior_container
 import rospy
+import actionlib
 
 from geometry_msgs.msg import Quaternion
 from move_base_behavior import MoveBaseBehavior
+from ipa_manipulation_msgs.msg import MoveToAction, MoveToGoal
+import services_params as srv
+from tf.transformations import quaternion_from_euler
 
 class TrashcanEmptyingBehavior(behavior_container.BehaviorContainer):
 
@@ -24,11 +28,16 @@ class TrashcanEmptyingBehavior(behavior_container.BehaviorContainer):
 		self.move_base_handler_ = MoveBaseBehavior("MoveBaseBehavior", self.interrupt_var_, move_base_service_str)
 		(self.trolley_position_, self.trashcan_position_) = (None, None)
 
+		self.catch_trashcan_service_str_ = srv.CATCH_TRASHCAN_SERVICE_STR
+		self.empty_trashcan_service_str_ = srv.EMPTY_TRASHCAN_SERVICE_STR
+		self.leave_trashcan_service_str_ = srv.LEAVE_TRASHCAN_SERVICE_STR
+		self.transport_position_service_str_ = srv.TRANSPORT_POSITION_STR
+		self.rest_position_service_str_ = srv.REST_POSITION_STR
+
 	# Method for setting parameters for the behavior
 	def setParameters(self, trashcan_position, trolley_position):
 		self.trashcan_position_ = trashcan_position
 		self.trolley_position_ = trolley_position
-
 
 	# Method for returning to the standard pose of the robot
 	def returnToRobotStandardState(self):
@@ -44,17 +53,45 @@ class TrashcanEmptyingBehavior(behavior_container.BehaviorContainer):
 		)
 		self.move_base_handler_.executeBehavior()
 
-	def takeTrashcan(self):
-		# todo (rmb-ma)
-		rospy.sleep(2.)
+	def catchTrashcan(self):
+		return self.executeAction(self.catch_trashcan_service_str_, position=[0.75,-0.60, 0.505], rotation=[0.0, 0.0, 3.92])
+
+
+	def executeAction(self, action_name, position=None, rotation=None):
+
+		goal = MoveToGoal()
+		if position is not None or rotation is not None:
+			goal = MoveToGoal()
+			orientation = quaternion_from_euler(*rotation)
+
+			goal.target_pos.pose.position.x = position[0]
+			goal.target_pos.pose.position.y = position[1]
+			goal.target_pos.pose.position.z = position[2]
+
+			goal.target_pos.pose.orientation.x = orientation[0]
+			goal.target_pos.pose.orientation.y = orientation[1]
+			goal.target_pos.pose.orientation.z = orientation[2]
+			goal.target_pos.pose.orientation.w = orientation[3]
+
+			goal.target_pos.header.frame_id = 'world'
+		client = actionlib.SimpleActionClient(action_name, MoveToAction)
+
+		client.wait_for_server()
+		client.send_goal(goal)
+		client.wait_for_result()
+		return client.get_result()
 
 	def emptyTrashcan(self):
-		# todo (rmb-ma)
-		rospy.sleep(2)
+		return self.executeAction(self.empty_trashcan_service_str_, position=[0.800, 0.600, 0.80], rotation=[-0., 0., 0.401])
 
 	def leaveTrashcan(self):
-		# todo (rmb-ma)
-		rospy.sleep(2)
+		return self.executeAction(self.leave_trashcan_service_str_, position=[0.75,-0.60, 0.505], rotation=[0.0, 0.0, 3.92])
+
+	def transportPosition(self):
+		return self.executeAction(self.transport_position_service_str_)
+
+	def restPosition(self):
+		return self.executeAction(self.rest_position_service_str_)
 
 	# Implemented Behavior
 	def executeCustomBehavior(self):
@@ -74,7 +111,12 @@ class TrashcanEmptyingBehavior(behavior_container.BehaviorContainer):
 			return
 
 		self.printMsg("> Todo. Take the trashcan")
-		self.takeTrashcan()
+		self.catchTrashcan()
+		if self.handleInterrupt() >= 1:
+			return
+
+		self.printMsg("> Going to transport position")
+		self.transportPosition()
 		if self.handleInterrupt() >= 1:
 			return
 
@@ -92,6 +134,11 @@ class TrashcanEmptyingBehavior(behavior_container.BehaviorContainer):
 		if self.handleInterrupt() >= 1:
 			return
 
+		self.printMsg("> Going to transport position")
+		self.transportPosition()
+		if self.handleInterrupt() >= 1:
+			return
+
 		self.printMsg("> Going to the trashcan location")
 		self.moveToGoal(self.trashcan_position_)
 		if self.handleInterrupt() >= 1:
@@ -106,3 +153,7 @@ class TrashcanEmptyingBehavior(behavior_container.BehaviorContainer):
 		if self.handleInterrupt() >= 1:
 			return
 
+		self.printMsg("> Going to rest position")
+		self.restPosition()
+		if self.handleInterrupt() >= 1:
+			return
